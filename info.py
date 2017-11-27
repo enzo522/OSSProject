9e969c7ef1f3da96f9349ca10e94a0eb49c6cc75
Sungsoo Kim <sungsoo522@naver.com>
2017-11-27 23:39:12 +0900

    일부 모듈화

---
+++ info.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import wx.grid
+from wx.grid import GridCellAutoWrapStringRenderer
+
+
+# InfoTable class to set information of selected video.
+class InfoTable(wx.grid.Grid):
+    def __init__(self, parent, dataList):
+        wx.grid.Grid.__init__(self, parent, -1)
+
+        self.CreateGrid(8, 1)
+        self.SetColLabelSize(1)
+        self.SetLabelBackgroundColour("white")
+        self.EnableEditing(False)
+
+        for i in range(8):
+            self.SetRowLabelValue(i, dataList[i][0])
+            self.SetCellValue(i, 0, dataList[i][1])
+            self.SetCellRenderer(i, 0, GridCellAutoWrapStringRenderer())
+
+        self.AutoSize()
+
+
+# VideoInfoDialog class to show information table which was made by InfoTable object.
+class VideoInfoDialog(wx.Dialog):
+    def __init__(self, video, x, y):
+        wx.Dialog.__init__(self, None, -1, "Info")
+        panel = wx.Panel(self)
+        self.SetBackgroundColour("white")
+        self.Bind(wx.EVT_CLOSE, self.__onClose)
+
+        tagList = [ "제   목", "저   자", "길   이", "평   점", "조회수", "좋아요", "싫어요", "설   명" ]
+        infoList = [ video.title, video.author, video.duration, round(video.rating, 1).__str__(), \
+                     video.viewcount.__str__(), video.likes.__str__(), video.dislikes.__str__(), video.description ]
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        dataList = []
+
+        for i in range(8):
+            data = (tagList[i], infoList[i])
+            dataList.append(data)
+
+        sizer.Add(InfoTable(self, dataList), flag=wx.ALL, border=10)
+        panel.SetSizerAndFit(sizer)
+        self.Fit()
+
+        if x > 0 and y > 0:
+            self.Move(self.GetPosition().__iadd__((x, y)))
+
+    def __onClose(self, event):
+        self.Destroy()
--- mainframe.py
+++ mainframe.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
-import pafy
 import wx
+import pafy
+from item import Item
+from info import VideoInfoDialog
 
 FRAME_WIDTH = 870
 FRAME_HEIGHT = 480
@@ -241,16 +242,17 @@ class MainFrame(wx.Frame):
 
         # add item to list
     def addToDownloadList(self, item):
-        self.__downloadList.append(item)
-        num_items = self.__addedList.GetItemCount()
+        if item not in self.__downloadList:
+            self.__downloadList.append(item)
+            num_items = self.__addedList.GetItemCount()
 
-        self.__addedList.InsertItem(num_items, item.video.title)
-        self.__addedList.SetItem(num_items, 1, item.video.author)
-        self.__addedList.SetItem(num_items, 2, item.video.duration)
-        self.__addedList.SetItem(num_items, 3, item.selectedExt)
-        self.__addedList.SetItem(num_items, 4, item.filesize)
+            self.__addedList.InsertItem(num_items, item.video.title)
+            self.__addedList.SetItem(num_items, 1, item.video.author)
+            self.__addedList.SetItem(num_items, 2, item.video.duration)
+            self.__addedList.SetItem(num_items, 3, item.selectedExt)
+            self.__addedList.SetItem(num_items, 4, item.filesize)
 
-        self.SetStatusText(item.video.title + " has been added.")
+            self.SetStatusText(item.video.title + " has been added.")
 
         # update status of downloading item
     def updateStatus(self, item, rate, progress, eta):
@@ -270,91 +272,9 @@ class MainFrame(wx.Frame):
         self.__downloading = False
 
 
-# Item class to encapsulate video and selected options (media type, extension, resolution)
-class Item():
-    def __init__(self, video, selectedExt):
-        self.video = video
-        self.selectedExt = selectedExt
-        self.filesize = None
-        self.__sizes = []
-        self.options = []
-
-        for s in self.video.allstreams:
-            self.options.append(s.mediatype + " / " + s.extension + " / " + s.quality)
-            self.__sizes.append(s.get_filesize())
-
-        self.__calcFilesize()
-
-    def __calcFilesize(self):
-        orgFilesize = None
-
-        for i in range(len(self.options)):
-            if self.selectedExt == self.options[i]:
-                orgFilesize = self.__sizes[i]
-
-        self.filesize = round(orgFilesize / 1024 ** 2, 1).__str__() + "MB" if orgFilesize > 1024 ** 2 else \
-            round(orgFilesize / 1024, 1).__str__() + "KB" \
-            if orgFilesize is not None else ""
-
-    def setSelectedExt(self, newExt):
-        self.selectedExt = newExt
-        self.__calcFilesize()
-
-
-import wx.grid
-from wx.grid import GridCellAutoWrapStringRenderer
-
-
-# InfoTable class to set information of selected video.
-class InfoTable(wx.grid.Grid):
-    def __init__(self, parent, dataList):
-        wx.grid.Grid.__init__(self, parent, -1)
-
-        self.CreateGrid(8, 1)
-        self.SetColLabelSize(1)
-        self.SetLabelBackgroundColour("white")
-        self.EnableEditing(False)
-
-        for i in range(8):
-            self.SetRowLabelValue(i, dataList[i][0])
-            self.SetCellValue(i, 0, dataList[i][1])
-            self.SetCellRenderer(i, 0, GridCellAutoWrapStringRenderer())
-
-        self.AutoSize()
-
-
-# VideoInfoDialog class to show information table which was made by InfoTable object.
-class VideoInfoDialog(wx.Dialog):
-    def __init__(self, video, x, y):
-        wx.Dialog.__init__(self, None, -1, "Info")
-        panel = wx.Panel(self)
-        self.SetBackgroundColour("white")
-        self.Bind(wx.EVT_CLOSE, self.__onClose)
-
-        tagList = [ "제   목", "저   자", "길   이", "평   점", "조회수", "좋아요", "싫어요", "설   명" ]
-        infoList = [ video.title, video.author, video.duration, round(video.rating, 1).__str__(), \
-                     video.viewcount.__str__(), video.likes.__str__(), video.dislikes.__str__(), video.description ]
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        dataList = []
-
-        for i in range(8):
-            data = (tagList[i], infoList[i])
-            dataList.append(data)
-
-        sizer.Add(InfoTable(self, dataList), flag=wx.ALL, border=10)
-        panel.SetSizerAndFit(sizer)
-        self.Fit()
-
-        if x > 0 and y > 0:
-            self.Move(self.GetPosition().__iadd__((x, y)))
-
-    def __onClose(self, event):
-        self.Destroy()
-
-
 from time import sleep
 import threading
+import os
 
 
 # AddManager class to add urls to download list.
@@ -370,12 +290,12 @@ class AddManager(threading.Thread):
             if self.__isRunning:
                 video = pafy.new(url)
 
-                if video.has_basic: # check current url is available
-                    default = video.getbest() # default selected options are the best ones that current video has
+                if video.has_basic:  # check current url is available
+                    default = video.getbest()  # default selected options are the best ones that current video has
 
                     with self._lock:
                         frame.addToDownloadList(Item(video, default.mediatype + " / " + default.extension + " / " + \
-                                             default.resolution))
+                                                     default.resolution))
 
                     sleep(WAIT_TIME)
 
@@ -394,7 +314,7 @@ class DownloadManager(threading.Thread):
     def __init__(self, itemList, dir):
         super(DownloadManager, self).__init__()
         self.__dir = dir
-        self.__queue = Queue(len(itemList)) # a queue to download in order
+        self.__queue = Queue(len(itemList))  # a queue to download in order
         self.__threadList = []
         self.__isRunning = True
         self.__abort = False
@@ -405,7 +325,7 @@ class DownloadManager(threading.Thread):
 
     def run(self):
         while self.__isRunning:
-            if len(self.__threadList) < 3: # download 3 videos simultaneously
+            if len(self.__threadList) < 3:  # download 3 videos simultaneously
                 if not self.__queue.empty():
                     dl = Downloader(self.__queue.get(), self.__dir, self.__abort)
                     self.__threadList.append(dl)
@@ -416,23 +336,23 @@ class DownloadManager(threading.Thread):
                 if not t.isAlive():
                     self.__threadList.remove(t)
                     break
-                else: # update download progress
+                else:  # update download progress
                     t.updateStatus()
                     sleep(WAIT_TIME)
 
-            if len(self.__threadList) <= 0 and self.__queue.empty(): # when every video is completed
+            if len(self.__threadList) <= 0 and self.__queue.empty():  # when every video is completed
                 self.__isRunning = False
 
         with self._lock:
             frame.setFinished()
 
-    def skip(self): # cancel current downloads
+    def skip(self):  # cancel current downloads
         for t in self.__threadList:
             if t.isAlive():
                 t.stop()
                 t.join()
 
-    def stop(self): # cancel current downloads and abort further ones
+    def stop(self):  # cancel current downloads and abort further ones
         self.skip()
         self.__abort = True
 
@@ -450,30 +370,30 @@ class Downloader(threading.Thread):
         self.__filename = downloadPath + self.__item.video.title + "."
         self._lock = threading.RLock()
 
-        for s in self.__item.video.allstreams: # find a stream which satisfies selected options
+        for s in self.__item.video.allstreams:  # find a stream which satisfies selected options
             if self.__item.selectedExt == s.mediatype + " / " + s.extension + " / " + s.quality:
                 self.__stream = s
                 break
 
-    def updateStatus(self): # current download progress about this video
+    def updateStatus(self):  # current download progress about this video
         if self.__stream.has_stats:
             stats = self.__stream.progress_stats
             rate = round(stats[0] * 100, 1).__str__() + "%"
             progress = round(stats[1] / 1024, 1).__str__() + "MB/s" if stats[1] > 1024 else \
                 round(stats[1], 1).__str__() + "KB/s"
             eta = round(stats[2], 1).__str__() + "초"
-            
+
             frame.updateStatus(self.__item, progress, rate, eta)
 
-    def run(self): # if the user clicked stop button, the downloader shouldn't start download
-        if not self.__abort: # otherwise, check whether the user has already downloaded this video
+    def run(self):  # if the user clicked stop button, the downloader shouldn't start download
+        if not self.__abort:  # otherwise, check whether the user has already downloaded this video
             if not os.path.exists(self.__filename + self.__stream.extension):
                 self.__stream.download(filepath=self.__filename + self.__stream.extension, quiet=True)
 
         with self._lock:
             frame.removeFinishedItem(self.__item)
 
-    def stop(self): # when the user clicked skip / stop button, current download should be canceled
+    def stop(self):  # when the user clicked skip / stop button, current download should be canceled
         if self.__stream:
             self.__stream.cancel()
 
